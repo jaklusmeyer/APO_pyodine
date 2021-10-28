57297804e8188d4e6a9360d18dbe0c9ec20e91bd
Heeren <ppheeren@posteo.de>
2021-10-28 23:58:44 +0200

    timeseries overall finished. thinking about using logging instead of the self-written function for writing to file and printing to the terminal. resume to testing now.

--- utilities_song/timeseries_parameters.py
+++ utilities_song/timeseries_parameters.py
@@ -5,48 +5,53 @@
     Paul Heeren, 8/03/2021
 """
 
-class TimeseriesParameters:
+class Timeseries_Parameters:
     
-    def __init__(self):
-        # General parameters:
-        self.diagnosis_out = True           # Whether to print and plot diagnosis to file
+    def __init__(self):        
         
-        # Chunk requirements:
-        self.min_counts = 1000.             # Minimum counts that a chunk should have!
-        self.max_counts = 1000000.           # Maximum counts that a chunk should have!
-        self.max_redchi2 = 100000000.           # Maximum red. Chi^2 a chunk should have! #58000
-        
-        # Second step to reject chunks (first step is with chauvenet criterion):
-        # Discard poor red. Chi2 chunks and those with large photon errors
-        # -> only use best 2 or 3 sigma (95.5 or 99.7 %)
-        self.rejection_percentile = 0.997   # Rejection percentile limit
-        
-        # Weight chunk series by their relative scatter around observation medians
-        self.default_sigma = 1000.          # Default sigma of chunk series with less than 4 good ones ([m/s])
-        
-        # Finally compute weighted observation velocities
-        # -> only use best 2 or 3 sigma (95.5 or 99.7 %)
-        self.useage_percentile = 0.997      # Useage percentile limit of chunks
-        
-        
-        
-        
-        
+        # If you hand a list of filenames to reject, are these the names of the
+        # individual modelling results ('res_files') or of the original 
+        # observations ('obs_files')?
         self.reject_type = 'res_files'
+        
+        # This dictionary defines the parameters used in the weighting
+        # algorithm: 
+        # - 'good_chunks' and 'good_orders' define which chunks to use in the
+        #   computation of observation means (to offset-correct all chunks)
+        # - 'sig_limits' defines the lower and upper limit in m/s that chunk
+        #   timeseries are allowed to have - outliers are corrected to 
+        #   'sig_correct';
+        # - 'reweight_alpha', 'reweight_beta' and 'reweight_sigma' are
+        #   used in the reweight function to create the chunk weights;
+        # - 'weight_correct' is the value that weights of 0 or NaN are 
+        #   corrected to.
         self.weighting_pars = {
+                'good_chunks': None, #(3, 15), #(150, 350)
+                'good_orders': None, #(6,14)
+                'sig_limits': [4., 1000.],
+                'sig_correct': 1000.,
                 'reweight_alpha': 1.8,
                 'reweight_beta': 8.0,
                 'reweight_sigma': 2.0,
                 'weight_correct': 0.01,
-                'sig_limits': [4., 1000.],
-                'sig_correct': 1000.,
-                'good_chunks': None, #(3, 15), #(150, 350)
-                'good_orders': None #(6,14)
                 }
+        
+        # Do chromatic index computation?
         self.do_crx = True
-        self.txt_outkeys = ['bary_date', 'rv', 'rv_err']
-        self.txt_delimiter = '\t'
-        self.txt_header = None
-        self.txt_outformat = ['%10.5f', '6.4f', '3.4f']
+        
+        # For writing timeseries results to a text-file:
+        self.txt_outkeys = ['bary_date', 'rv', 'rv_err']    # Write these results
+        self.txt_delimiter = '\t'                           # Delimiter to use
+        self.txt_header = None                              # Header line
+        self.txt_outformat = ['%10.5f', '6.4f', '3.4f']     # Output format (make sure
+                                                            # this matches the keys!)
+        
+        # Save the final results to file?
+        self.save_comb_res = True
+        
+        # Create and save analysis plots?
         self.plot_analysis = True
+        
+        # Print observation names of RV outliers? (Only if plot_analysis is True!)
+        self.print_outliers = True
         
