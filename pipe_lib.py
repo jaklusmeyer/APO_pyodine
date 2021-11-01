Tested observation modelling and velocity combination/weighting, using Tutorial Jupyter Notebooks. Many things corrected and improved.

pipe_lib.py
@@ -330,13 +330,12 @@ def create_analysis_plots(fit_results, save_dir, run_id=None, tellurics=None,
         print('Run results analysis failed...')
 
 
-def velocity_results_analysis(fit_results, save_dir, nr_chunks_order, 
+def velocity_results_analysis(run_result, save_dir, nr_chunks_order, 
                               nr_orders, obs_filename):
     """Perform a short analysis of velocity results
     
-    :param fit_results: A list containing instances of :class:`LmfitResult`
-        for each chunk.
-    :type fit_results: list[:class:`LmfitResult`]
+    :param run_result: Then results dictionary of the final modelling run.
+    :type run_result: dict
     :param save_dir: The directory name where to save plots.
     :type save_dir: str
     :param nr_chunks_order: Number of chunks per order. If this and nr_orders 
@@ -357,7 +356,7 @@ def velocity_results_analysis(fit_results, save_dir, nr_chunks_order,
         # Process and print overview of results
         obs_params = []
         obs_errors = []
-        for r in fit_results:
+        for r in run_result['results']:
             obs_params.append(r.params)
             obs_errors.append(r.errors)
         
@@ -459,12 +458,12 @@ def velocity_results_analysis(fit_results, save_dir, nr_chunks_order,
                                  format='png', dpi=300)
         plt.close()
         
-        # Wavelengths (residuals to fit) phase-folded over orders
+        # Wavelengths (residuals to fit) phase-folded over orders        
         fig = plt.figure(figsize=(12,6))
         for i in range(nr_orders):
             plt.plot(np.arange(nr_chunks_order), 
-                     [fit_results[j].params['wave_intercept'] for j in range(i*nr_chunks_order,(i+1)*nr_chunks_order)] - \
-                     fit_results['wave_intercept_fit'][i*nr_chunks_order:(i+1)*nr_chunks_order],
+                     [obs_params[j]['wave_intercept'] for j in range(i*nr_chunks_order,(i+1)*nr_chunks_order)] - \
+                     run_result['wave_intercept_fit'][i*nr_chunks_order:(i+1)*nr_chunks_order],
                      '-', alpha=0.5)
         plt.ylim(-0.02,0.02)
         plt.xlabel('Chunk # within order')

pyodine/fitters/results_io.py
@@ -3,6 +3,11 @@ import numpy as np
 from ..lib import h5quick
 import dill
 import os
+from ..components import ChunkArray, SummedObservation
+from ..template.base import StellarTemplate_Chunked
+from ..models import lsf, wave, cont, spectrum
+from ..fitters.lmfit_wrapper import LmfitWrapper
+from .. import chunks
 
 _group_keys = ('observation', 'chunks', 'params', 'errors', 'model')
 _array_keys = ('reports', 'redchi2', 'residuals', 'medcnts')
@@ -45,7 +50,8 @@ def create_results_dict(fit_results):
     res_dict['model'] = {
             'lsf_model': fit_results[0].model.lsf_model.name().encode('utf8', 'replace'),
             'iodine_file': os.path.abspath(fit_results[0].model.iodine_atlas.orig_filename).encode('utf8', 'replace'),
-            'osample_factor': fit_results[0].model.osample_factor
+            'osample_factor': fit_results[0].model.osample_factor,
+            'lsf_conv_width': fit_results[0].model.conv_width
             }
     # If not a fit result from O-star modelling
     if fit_results[0].model.stellar_template is not None:
@@ -66,7 +72,7 @@ def create_results_dict(fit_results):
     # Collect info from all chunks
     res_dict['reports']   = np.array([res.report for res in fit_results], dtype='S')
     res_dict['redchi2']   = np.array([res.redchi for res in fit_results])
-    res_dict['residuals'] = np.array([res.residuals for res in fit_results])
+    res_dict['residuals'] = np.array([res.rel_residuals_rms() for res in fit_results])
     res_dict['medcnts']   = np.array([res.medcnts for res in fit_results])
     
     res_dict['chunks'] = {k: np.zeros(nchunk, dtype='int') for k in ('order', 'firstpix', 'lastpix', 'padding')}
@@ -203,95 +209,11 @@ def save_results(filename, fit_results, filetype='h5py'):
                 h5quick.dict_to_group(res_dict[key], h, key)
             
             for key in _array_keys:
-                #print(key)
-                #print(res_dict[key])
-                #h5quick.dict_to_group(res_dict[key], h, key)
                 h[key] = res_dict[key]
-            """
-            h5quick.dict_to_group(res_dict['observation'], h, 'observation')
-            h5quick.dict_to_group(res_dict['chunks'], h, 'chunks')
-            h5quick.dict_to_group(res_dict['params'], h, 'params')
-            h5quick.dict_to_group(res_dict['errors'], h, 'errors')
-            h['reports'] = reports
-            h['redchi2'] = redchi2
-            h['residuals'] = residuals
-            h['medcounts'] = medcnts
-            h5quick.dict_to_group(modinfo, h, 'model')
-            """
     
     else:
         raise KeyError('The savetype must be either of "h5py" or "dill",' + \
-                       'but {} was supplied!'.format(filetype))
-        
-        """
-        # Collect observation info
-        # Note: Unicode strings are currently not supported by h5py, so we need to
-        # convert to bytestring (ascii). Special characters are replaced with
-        # question marks
-        obs = fit_results[0].chunk.observation
-        obsinfo = {
-            'instrument_name': obs.instrument.name.encode('utf8', 'replace'),
-            'star_name': obs.star.name.encode('utf8', 'replace'),
-            'orig_header': obs.orig_header.tostring(sep='\n').encode('utf8', 'replace'),
-            'time_start': obs.time_start.isot.encode('utf8', 'replace'),
-            'bary_date': obs.bary_date,
-            'bary_vel_corr': obs.bary_vel_corr,
-        }
-        
-        # Collect modelling info
-        modinfo = {
-                'lsf_model': fit_results[0].model.lsf_model.name().encode('utf8', 'replace'),
-                'iodine_file': fit_results[0].model.iodine_atlas.orig_filename.encode('utf8', 'replace'),
-                'osample_factor': fit_results[0].model.osample_factor
-                }
-        # If not a fit result from O-star modelling
-        if fit_results[0].model.stellar_template is not None:
-            # Include the template info and the original filename of the observation
-            modinfo['stellar_template'] = fit_results[0].model.stellar_template.orig_filename.encode('utf8', 'replace')
-            obsinfo['orig_filename'] = obs.orig_filename.encode('utf8', 'replace')
-        else:
-            # Include the original filenames of all modelled O-star observations (if more than one)
-            if hasattr(obs, 'all_filenames'):
-                obsinfo['orig_filename'] = [f.encode('utf8', 'replace') for f in obs.all_filenames]
-            else:
-                obsinfo['orig_filename'] = obs.orig_filename.encode('utf8', 'replace')
-    
-        # Assume parameter names are the same in all elements of input array
-        param_names = list(fit_results[0].params.keys())
-        nchunk = len(fit_results)
-    
-        # Collect info from all chunks
-        chunks = {k: np.zeros(nchunk, dtype='int')
-                  for k in ('order', 'firstpix', 'lastpix', 'padding')}
-        params = {k: np.zeros(nchunk, dtype='float64')
-                  for k in param_names}
-        errors = {k: np.zeros(nchunk, dtype='float64')
-                  for k in param_names}
-        reports = []
-        redchi2 = []
-        residuals = []
-        medcnts = []
-        for i in range(nchunk):
-            res = fit_results[i]
-            # Get chunk info
-            chunks['order'][i] = res.chunk.order
-            chunks['firstpix'][i] = res.chunk.abspix[0]
-            chunks['lastpix'][i] = res.chunk.abspix[-1]
-            chunks['padding'][i] = res.chunk.padding
-            # Get parameter values and errors
-            for p in param_names:
-                params[p][i] = res.params[p]
-                errors[p][i] = res.errors[p]
-            reports += [res.report]
-            redchi2 += [res.redchi]
-            residuals += [res.rel_res_mean()]
-            medcnts += [res.medcounts]
-        reports = np.array(reports, dtype='S')
-        redchi2 = np.array(redchi2)
-        residuals = np.array(residuals)
-        medcnts = np.array(medcnts)
-        """
-        
+                       'but {} was supplied!'.format(filetype))        
     
 
 def load_results(filename, filetype='h5py', force=True):
@@ -355,82 +277,153 @@ def load_results(filename, filetype='h5py', force=True):
                 filename, filetype))
     
 
-def save_template_results(filename, fit_results):
-    """Preliminary function to save a set of results from the template creation
+def restore_results_object(utilities, filename):
+    """A wrapper function to restore saved results as 
+    :class:`LmfitWrapper.LmfitResult` objects
     
-    This is essentially the same as save_results(), except for the missing
-    template path name (obviously). So pull both together?! -> included now
-    up there. Let's test it.
+    If the results are saved as '.pkl' (dill format), this is straight forward.
+    If the save format however is '.h5' (HDF5/h5py), it's more complicated as
+    the objects need to be created from scratch, using the information 
+    contained in the saved dictionary. IMPORTANT: This only works if all 
+    pathnames of the input files (observation spectra, I2 atlas, templates) 
+    have not changed!
     
-    If 'filename' exists, it will be overwritten.
+    :param utilities: The utilities module for the instrument used.
+    :type utilities: library
+    :param filename: The filename of the saved results.
+    :type filename: str
     
-    Args:
-        filename (str): Output path for the results file.
-        fit_results (list): A list of :class:'LmfitWrapper.LmfitResult' objects.
+    :return: The chunks of the modelled observation.
+    :rtype: :class:`ChunkArray`
+    :return: The list of fit results of the modelled observation.
+    :rtype: list
     """
     
-    # Collect observation info
-    # Note: Unicode strings are currently not supported by h5py, so we need to
-    # convert to bytestring (ascii). Special characters are replaced with
-    # question marks
-    obs = fit_results[0].chunk.observation
-    obsinfo = {
-        'instrument_name': obs.instrument.name.encode('utf8', 'replace'),
-        'star_name': obs.star.name.encode('utf8', 'replace'),
-        'orig_filename': obs.orig_filename.encode('utf8', 'replace'),
-        'orig_header': obs.orig_header.tostring(sep='\n').encode('utf8', 'replace'),
-        'time_start': obs.time_start.isot.encode('utf8', 'replace'),
-        'bary_date': obs.bary_date,
-        'bary_vel_corr': obs.bary_vel_corr,
-    }
+    # First check the filetype of the filename
+    filetype = filetype_from_ext(filename)
+    results = load_results(filename, filetype)
     
-    # Collect modelling info
-    modinfo = {
-            'lsf_model': fit_results[0].model.lsf_model.name().encode('utf8', 'replace'),
-            'iodine_file': fit_results[0].model.iodine_atlas.orig_filename.encode('utf8', 'replace'),
-            'osample_factor': fit_results[0].model.osample_factor
-            }
-
-    # Assume parameter names are the same in all elements of input array
-    param_names = list(fit_results[0].params.keys())
-    nchunk = len(fit_results)
-
-    # Collect info from all chunks
-    chunks = {k: np.zeros(nchunk, dtype='int')
-              for k in ('order', 'firstpix', 'lastpix', 'padding')}
-    params = {k: np.zeros(nchunk, dtype='float64')
-              for k in param_names}
-    errors = {k: np.zeros(nchunk, dtype='float64')
-              for k in param_names}
-    reports = []
-    redchi2 = []
-    medcnts = []
-    for i in range(nchunk):
-        res = fit_results[i]
-        # Get chunk info
-        chunks['order'][i] = res.chunk.order
-        chunks['firstpix'][i] = res.chunk.abspix[0]
-        chunks['lastpix'][i] = res.chunk.abspix[-1]
-        chunks['padding'][i] = res.chunk.padding
-        # Get parameter values and errors
-        for p in param_names:
-            params[p][i] = res.params[p]
-            errors[p][i] = res.errors[p]
-        reports += [res.report]
-        redchi2 += [res.redchi]
-        medcnts += [res.medcounts]
-    reports = np.array(reports, dtype='S')
-    redchi2 = np.array(redchi2)
-    medcnts = np.array(medcnts)
-
-    # Save to HDF5
-    with h5py.File(filename, 'w') as h:
-        h5quick.dict_to_group(obsinfo, h, 'observation')
-        h5quick.dict_to_group(chunks, h, 'chunks')
-        h5quick.dict_to_group(params, h, 'params')
-        h5quick.dict_to_group(errors, h, 'errors')
-        h['reports'] = reports
-        h['redchi2'] = redchi2
-        h['medcnts'] = medcnts
-        h5quick.dict_to_group(modinfo, h, 'model')
+    # If it was a 'dill' file, the result should already be recovered as
+    # object structure and we only need to create the chunk array
+    if not isinstance(results, dict):
+        obs_chunks = ChunkArray()
+        for r in results:
+            obs_chunks.append(r.chunk)
+        
+        return obs_chunks, results
+    
+    # For a 'h5py' file in contrast, we need to try and manually rebuild the
+    # object structure from the information in the saved dictionary
+    else:
+        
+        # First get the names of the observation(s) (this could be more than
+        # one, in case several observation spectra were summed up)
+        if isinstance(results['observation']['orig_filename'], (list,np.ndarray)):
+            obs_path = [f.decode() for f in results['observation']['orig_filename']]
+        else:
+            obs_path = [results['observation']['orig_filename'].decode()]
+        
+        # Now the stellar template name (if any - for hot star modelling during
+        # template creation this is None)
+        if 'stellar_template' in results['model'].keys():
+            temp_path = results['model']['stellar_template'].decode()
+        else:
+            temp_path = None
+        
+        # Now other important information: The pathname to the I2 atlas, the
+        # used oversampling factor, the LSF convolution width, the name of the 
+        # used LSF, and info about the chunks and best-fit parameters
+        iod_path       = results['model']['iodine_file'].decode()
+        osample        = results['model']['osample_factor']
+        lsf_conv_width = results['model']['lsf_conv_width']
+        lsf_name       = results['model']['lsf_model'].decode()
+        res_chunks     = results['chunks']
+        res_params     = results['params']
+        
+        # The orders covered by the chunks, the width of the chunks, and the
+        # padding of the chunks (these are constructed from implicit 
+        # information in the res_chunks dictionary)
+        orders  = np.unique(np.array([o for o in res_chunks['order']]))
+        # Chunk 2 should really not be affected by order edge effects
+        width   = abs(res_chunks['lastpix'][2]-res_chunks['firstpix'][2])+1
+        padding = res_chunks['padding'][2]
+        
+        # Load the observation data (either single one or multiple summed up)
+        all_obs = [utilities.load_pyodine.ObservationWrapper(f) for f in obs_path]
+        if len(obs_path) > 1:
+            obs = SummedObservation(*all_obs)
+        else:
+            obs = all_obs[0]
+        
+        # If a template was used, load it also
+        # ToDo: Allow also for 'StellarTemplate' object?
+        if temp_path:
+            temp = StellarTemplate_Chunked(temp_path)
+        else:
+            temp = None
+        
+        # Load the I2 atlas
+        iod = utilities.load_pyodine.IodineTemplate(iod_path)
+        
+        # Build the model and fitter
+        lsf_model  = lsf.model_index[lsf_name]
+        wave_model = wave.LinearWaveModel
+        cont_model = cont.LinearContinuumModel
+        model      = spectrum.SimpleModel(
+                lsf_model, wave_model, cont_model, iod, stellar_template=temp, 
+                osample_factor=osample, conv_width=lsf_conv_width)
+        
+        # Initialize the fitter object
+        fitter = LmfitWrapper(model)
+        
+        # Build the chunk array: If a template is given, use the wave_defined
+        # algorithm, otherwise the user_defined function (default functions
+        # in the standard pyodine distribution)
+        if temp:
+            # Compute possible order shifts between template and observation,
+            # by searching for the best coverage of first template order in
+            # observation
+            obs_order_min, min_coverage = obs.check_wavelength_range(
+                    temp[0].w0, temp[len(temp.get_order_indices(temp.orders_unique[0]))-1].w0)
+            order_correction = obs_order_min - temp.orders_unique[0]
+            print('Order correction: {}\n'.format(order_correction))
+            
+            obs_chunks = chunks.wave_defined(obs, temp, width=width, orders=orders-order_correction, 
+                                             padding=padding, order_correction=order_correction)
+        else:
+            chunks_per_order = len(np.where(res_chunks['order'] == orders[0])[0])
+            pix_offset0      = res_chunks['firstpix'][0]
+            
+            obs_chunks = chunks.user_defined(obs, width=width, orders=orders, 
+                                             padding=padding, chunks_per_order=chunks_per_order, 
+                                             pix_offset0=pix_offset0)
+        
+        # The total number of chunks, and the number of chunks per order
+        nr_chunks_total = len(obs_chunks)
+        nr_chunks       = len(obs_chunks.get_order(orders[0]))
+        
+        print('Total number of created chunks: {} (in result file: {})'.format(
+                nr_chunks_total, len(res_chunks['order'])))
+        print('Number of created chunks per order: {}'.format(nr_chunks))
+        
+        # Loop over the chunks to build the fit_result object
+        fit_results = []
+        for i, chunk in enumerate(obs_chunks):
+            # First guess the chunk parameters to create the ParameterSet object
+            pars = model.guess_params(chunk)
+            
+            # Then fill it with the best-fit results for that chunk
+            for key in res_params.keys():
+                pars[key] = res_params[key][i]
+            
+            # Convert it to an LM-fit parameter object, fix all the parameters
+            lmfit_pars = fitter.convert_params(pars, to_lmfit=True)
+            for key in lmfit_pars.keys():
+                lmfit_pars[key].set(vary=False)
+            
+            # Finally 'fit' it (work-around to create a fully functional 
+            # FitResults object)
+            fit_results.append(fitter.fit(chunk, lmfit_pars, chunk_ind=i))
+        
+        return obs_chunks, fit_results
         

pyodine/timeseries/combine_vels.py
@@ -21,7 +21,8 @@ _weighting_pars = {
         'reweight_beta': 8.0,
         'reweight_sigma': 2.0,
         'weight_correct': 0.01,
-        'sig_limits': [4., 1000.],
+        'sig_limit_low': 4., 
+        'sig_limit_up': 1000.,
         'sig_correct': 1000.,
         'good_chunks': None, #(3, 15), #(150, 350)
         'good_orders': None #(6,14)
@@ -77,7 +78,7 @@ def combine_chunk_velocities(velocities, nr_chunks_order, bvc=None,
     if not isinstance(weighting_pars, dict):
         pars = _weighting_pars
     else:
-        pars = weighting_pars
+        pars = weighting_pars.copy()
     
     printLog(diag_file, '---------------------------------------------------')
     printLog(diag_file, '- Pyodine chunk combination (based on iSONG code) -')
@@ -121,6 +122,8 @@ def combine_chunk_velocities(velocities, nr_chunks_order, bvc=None,
             good_ind += [(i + nr_chunks_order*o) for i in range(pars['good_chunks'][0], pars['good_chunks'][1]+1)]
     else:
         good_ind = [i for i in range(len(velocities))]
+        del pars['good_orders']
+        del pars['good_chunks']
     
     vel_offset_corrected = np.zeros((nr_obs, nr_chunks))
     for i in range(nr_obs):
@@ -161,7 +164,7 @@ def combine_chunk_velocities(velocities, nr_chunks_order, bvc=None,
         dev[i,:] = dev[i,:] - robust_mean(dev[i,:])
     
     # Finally, set very low and very high sigmas to a pre-defined value
-    ind = np.where(np.logical_or(sig<=pars['sig_limits'][0], sig>=pars['sig_limits'][1]))
+    ind = np.where(np.logical_or(sig<=pars['sig_limit_low'], sig>=pars['sig_limit_up']))
     sig[ind] = pars['sig_correct']
     
     # Print to file
@@ -180,13 +183,13 @@ def combine_chunk_velocities(velocities, nr_chunks_order, bvc=None,
             }
     if isinstance(wavelengths, (list,tuple,np.ndarray)):
         rv_dict['crx'] = np.zeros(nr_obs)           # The chromatic index in each observation
-        rv_dict['crx_err'] = np.zeros(nr_obs),      # The fit errors of the chromatic indices
-        rv_dict['RV_wave'] = np.zeros(nr_obs),      # The effect. wavelengths at the weighted RVs
-        rv_dict['RV_wave_err'] = np.zeros(nr_obs),  # The fit errors of the effect. wavelengths
+        rv_dict['crx_err'] = np.zeros(nr_obs)       # The fit errors of the chromatic indices
+        rv_dict['RV_wave'] = np.zeros(nr_obs)       # The effect. wavelengths at the weighted RVs
+        rv_dict['RV_wave_err'] = np.zeros(nr_obs)   # The fit errors of the effect. wavelengths
         rv_dict['crx_redchi'] = np.zeros(nr_obs)    # The red. Chi**2 of the crx fits
         
     
-    chunk_weights = np.zeros((nr_obs,nr_chunks)),   # The corrected weights for each individual chunk 
+    chunk_weights = np.zeros((nr_obs,nr_chunks))    # The corrected weights for each individual chunk 
                                                     # (after the reweight function)
     
     # Now loop over the observations to compute the final results
@@ -236,18 +239,21 @@ def combine_chunk_velocities(velocities, nr_chunks_order, bvc=None,
         # ToDo: Use weights?!
         if isinstance(wavelengths, (list,tuple,np.ndarray)):
             crx_dict = chromatic_index_observation(
-                    chunk_vels_corr, wavelengths[i], rv_dict['rv'][i])
+                    #vel_offset_corrected[i], wavelengths[i], rv_dict['rv'][i], weights=chunk_weights[i])
+                    #chunk_vels_corr, wavelengths[i], rv_dict['rv'][i], weights=chunk_weights[i])
+                    velocities[i,:], wavelengths[i], rv_dict['rv'][i], weights=chunk_weights[i])
+            
             for key, value in crx_dict.items():
-                rv_dict[key][i] = crx_dict[key]
+                rv_dict[key][i] = value
         
     # Some metrics of the quality of the RV timeseries
     rv_precision1 = np.sqrt(1./np.nansum(1./sig**2.))
     rv_precision2 = np.sqrt(1./np.nansum(np.nanmedian(chunk_weights[i], axis=0)))
     
     printLog(diag_file, 'RV quality factor 1 ( sqrt(1/sum(1/sig**2)) ): {} m/s'.format(
-            rv_dict['rv_precision1']))
+            rv_precision1))
     printLog(diag_file, 'RV quality factor 2 ( sqrt(1/sum(med(wt1))) ): {} m/s'.format(
-            rv_dict['rv_precision2']))
+            rv_precision2))
     
     auxiliary_dict = {
             'chunk_sigma': sig,
@@ -327,13 +333,25 @@ def chromatic_index_observation(velocities, wavelengths, RV, weights=None):
         
         velocities_fit = velocity_from_chromatic_index(
                 wavelengths, RV, lmfit_params['RV_wave'], lmfit_params['crx'])
+        if len(np.where(np.isnan(velocities_fit))[0]) > 0:
+            print(velocities_fit)
         if isinstance(weights, (list,np.ndarray)):
             return (velocities_fit - velocities) * np.sqrt(np.abs(weights))
         else:
             return velocities_fit - velocities
     
-    # First a simple polynomial fit to get a first estimate
-    #velocities_fit, coeffs = fit_polynomial(ln_wave, velocities, deg=1)
+    # Take care of NaNs in the wavelengths or velocities arrays: Exclude them
+    w_nan_inds = np.where(np.isnan(wavelengths))
+    if len(w_nan_inds[0]) > 0:
+        w_fin_inds = np.where(np.isfinite(wavelengths))
+        wavelengths = wavelengths[w_fin_inds]
+        velocities = velocities[w_fin_inds]
+        
+    v_nan_inds = np.where(np.isnan(velocities))
+    if len(v_nan_inds[0]) > 0:
+        v_fin_inds = np.where(np.isfinite(velocities))
+        wavelengths = wavelengths[v_fin_inds]
+        velocities = velocities[v_fin_inds]
     
     # Now do the parameter starting guesses
     # For the effective wavelength of the modelled RV:
@@ -351,16 +369,17 @@ def chromatic_index_observation(velocities, wavelengths, RV, weights=None):
     
     # Set up the the lmfit parameters
     lmfit_params = lmfit.Parameters()
-    lmfit_params.add(('RV_wave', RV_wave_guess))
-    lmfit_params.add(('crx', crx_guess))
+    lmfit_params.add('RV_wave', value=RV_wave_guess, min=np.min(wavelengths), 
+                     max=np.max(wavelengths))
+    lmfit_params.add('crx', value=crx_guess)
     
     # And fit
     lmfit_result = lmfit.minimize(func, lmfit_params, args=[wavelengths, velocities, RV, weights])
     
     crx_dict = {
-            'crx': lmfit_result.params['crx'], 
+            'crx': lmfit_result.params['crx'].value,
             'crx_err': lmfit_result.params['crx'].stderr, 
-            'RV_wave': lmfit_result.params['RV_wave'], 
+            'RV_wave': lmfit_result.params['RV_wave'].value, 
             'RV_wave_err': lmfit_result.params['RV_wave'].stderr, 
             'crx_redchi': lmfit_result.redchi
             }

pyodine_combine_vels.py
@@ -120,6 +120,8 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
     elif isinstance(reject_files, str):
         with open(reject_files, 'r') as f:
             reject_names = [l.strip() for l in f.readlines()]
+    else:
+        reject_names = None
     
     
     ###########################################################################
@@ -155,11 +157,13 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
         # Plot velocity results
         fig = plt.figure(figsize=(10,6))
         plt.errorbar(Results.bary_date, Results.rv_bc, yerr=Results.rv_err, 
-                     fmt='.', alpha=0.7, label='Weighted velocities\nstd={:.2f} m/s'.format(
-                             np.nanstd(Results.rv_bc)))
+                     fmt='.', alpha=0.7, label='Weighted velocities:\n{:.2f}+-{:.2f} m/s'.format(
+                             robust_mean(Results.rv_bc),
+                             robust_std(Results.rv_bc)))
         plt.plot(Results.bary_date, Results.mdvel+Results.bary_vel_corr, 
-                 '.', alpha=0.5, label='Median velocities\nstd={:.2f} m/s'.format(
-                         np.nanstd(Results.mdvel+Results.bary_vel_corr)))
+                 '.', alpha=0.5, label='Median velocities:\n{:.2f}+-{:.2f} m/s'.format(
+                         robust_mean(Results.mdvel+Results.bary_vel_corr),
+                         robust_std(Results.mdvel+Results.bary_vel_corr)))
         plt.legend()
         plt.xlabel('JD')
         plt.ylabel('RV [m/s]')
@@ -177,9 +181,13 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
         
         fig = plt.figure(figsize=(10,6))
         plt.errorbar(bjd_good, rv_good, yerr=rv_err_good, fmt='.', alpha=0.7,
-                     label='Weighted velocities\nstd={:.2f} m/s)'.format(np.nanstd(rv_good)))
+                     label='Weighted velocities:\n{:.2f}+-{:.2f} m/s'.format(
+                             robust_mean(rv_good),
+                             robust_std(rv_good)))
         plt.plot(bjd_good, mdvel_good+bvc_good, '.', alpha=0.5,
-                 label='Median velocities\nstd={:.2f} m/s)'.format(np.nanstd(mdvel_good+bvc_good)))
+                 label='Median velocities:\n{:.2f}+-{:.2f} m/s'.format(
+                         robust_mean(mdvel_good+bvc_good),
+                         robust_std(mdvel_good+bvc_good)))
         plt.legend()
         plt.xlabel('JD')
         plt.ylabel('RV [m/s]')
@@ -197,7 +205,9 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
         # Plot chunk-to-chunk scatter of observations
         fig = plt.figure(figsize=(10,6))
         plt.plot(Results.bary_date, Results.c2c_scatter, '.', alpha=0.7, 
-                 label='std={:.2f} m/s'.format(np.nanstd(Results.c2c_scatter)))
+                 label='Mean: {:.2f}+-{:.2f} m/s'.format(
+                         robust_mean(Results.c2c_scatter),
+                         robust_std(Results.c2c_scatter)))
         plt.legend()
         plt.xlabel('JD')
         plt.ylabel('Chunk scatter [m/s]')
@@ -207,10 +217,10 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
         
         # Plot of chunk sigmas
         fig = plt.figure(figsize=(10,6))
-        plt.plot(Results.auxiliary['sigma'], '.', alpha=0.7, 
+        plt.plot(Results.auxiliary['chunk_sigma'], '.', alpha=0.7, 
                  label='Mean: {:.2f}+-{:.2f} m/s'.format(
-                         robust_mean(Results.auxiliary['sigma']), 
-                         robust_std(Results.auxiliary['sigma'])))
+                         robust_mean(Results.auxiliary['chunk_sigma']), 
+                         robust_std(Results.auxiliary['chunk_sigma'])))
         plt.legend()
         plt.xlabel('Chunks')
         plt.ylabel('Chunk sigmas [m/s]')
@@ -235,7 +245,7 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
         vel_corrected = Results.params['velocity'] - Results.auxiliary['chunk_offsets']
         vel_corrected = vel_corrected.T + Results.bary_vel_corr
         
-        fig = plt.figure(figsize=(10,10))
+        fig = plt.figure(figsize=(12,10))
         plt.imshow(vel_corrected, aspect='auto')
         plt.colorbar()
         plt.xlabel('Observations')
@@ -245,7 +255,7 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
         plt.close()
         
         # 3D plot of chunk deviations
-        fig = plt.figure(figsize=(10,10))
+        fig = plt.figure(figsize=(12,10))
         plt.imshow(Results.auxiliary['chunk_dev'].T, aspect='auto')
         plt.colorbar()
         plt.xlabel('Observations')
@@ -257,7 +267,7 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
         # Histogram of the final velocity weights
         fig = plt.figure(figsize=(10,6))
         plt.hist(Results.auxiliary['chunk_weights'].flatten(), bins=100, alpha=0.7,
-                 label=r'Mean: {}+-{} (m/s)$^{-2}$'.format(
+                 label='Mean: {:.3e}+-{:.3e} (m/s)$^{{-2}}$'.format(
                          robust_mean(Results.auxiliary['chunk_weights']), 
                          robust_std(Results.auxiliary['chunk_weights'])))
         plt.legend()
@@ -270,8 +280,9 @@ def combine_velocity_results(Pars, res_files=None, comb_res_in=None,
         if Pars.do_crx:
             fig = plt.figure(figsize=(10,6))
             plt.errorbar(Results.bary_date, Results.crx, yerr=Results.crx_err, 
-                         fmt='.', alpha=0.7, label='std={:.2f} m/s'.format(
-                                 np.nanstd(Results.crx)))
+                         fmt='.', alpha=0.7, label='Mean: {:.2f}+-{:.2f} (m/s)/Np'.format(
+                                 robust_mean(Results.crx), 
+                                 robust_std(Results.crx)))
             plt.legend()
             plt.xlabel('JD')
             plt.ylabel('CRX [(m/s)/Np]')