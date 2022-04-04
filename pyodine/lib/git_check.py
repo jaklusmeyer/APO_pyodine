de4a6353289f0103f70ae4e9a9905440c56e34f7
Heeren <ppheeren@posteo.de>
2022-04-04 10:05:55 +0200

    Corrected a bug in the git check library: It aimed to find the git info in the current working directory, not the pyodine repo. Now, by default, it executes the involved subprocess command in its own directory (which sits inside the pyodine repo).

--- pyodine/lib/git_check.py
+++ pyodine/lib/git_check.py
@@ -9,13 +9,40 @@ commit hash.
 """
 
 
-import subprocess
+import subprocess, os
+import logging
 
-def get_git_revision_hash() -> str:
-    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
 
-def get_git_revision_short_hash() -> str:
-    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+def get_git_revision_hash(pathname=None) -> str:
+    if not isinstance(pathname, str):
+        dirname = os.path.dirname(os.path.abspath(__file__))
+    else:
+        dirname = pathname
+    try:
+        return subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=dirname).decode('ascii').strip()
+    except Exception as e:
+        logging.error('git revision hash failed:', exc_info=True)
+        return 'git revision hash failed:\n' + str(e.args)
 
-def get_git_branch_name() -> str:
-    return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], universal_newlines=True).strip()
+def get_git_revision_short_hash(pathname=None) -> str:
+    if not isinstance(pathname, str):
+        dirname = os.path.dirname(os.path.abspath(__file__))
+    else:
+        dirname = pathname
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=dirname).decode('ascii').strip()
+    except Exception as e:
+        logging.error('git revision short hash failed:', exc_info=True)
+        return 'git revision short hash failed:\n' + str(e.args)
+        
+
+def get_git_branch_name(pathname=None) -> str:
+    if not isinstance(pathname, str):
+        dirname = os.path.dirname(os.path.abspath(__file__))
+    else:
+        dirname = pathname
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=dirname, universal_newlines=True).strip()
+    except Exception as e:
+        logging.error('git branch name failed:', exc_info=True)
+        return 'git branch name failed:\n' + str(e.args[0])
